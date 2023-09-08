109e40dbf48bd3af88334e570f04757158ff40ba
alantothe <rubenamalpartida@gmail.com>
2023-09-08 18:52:07 -0400

    redux folders update & pages folder created

--- src/app/page.js
+++ src/app/page.js
@@ -2,25 +2,12 @@
 
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  increment,
-  decrement,
-  incrementByAmount,
-} from "./redux/features/counter/counterSlice";
 
 function Home() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
-  return (
-    <div className="flex-col items-center justify-center">
-      <h1> Testing Out Redux</h1>
-      <h1>{count}</h1>
-      <button onClick={() => dispatch(increment())}>Add 1 </button>
-      <br />
-      <button onClick={() => dispatch(decrement())}>Subtract 1 </button>
-    </div>
-  );
+  return <div className="flex-col items-center justify-center"></div>;
 }
 
 export default Home;
--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./features/counter/counterSlice.js";
+import counterReducer from "./features/user/userSlice.js";
 
 export const store = configureStore({
   reducer: {
