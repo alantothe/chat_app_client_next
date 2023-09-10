78dbd98c334e0ca2fe400bbc58f5342e449e7b40
alantothe <rubenamalpartida@gmail.com>
2023-09-09 20:07:18 -0400

    add login thunk

--- src/app/redux/features/user/userThunks.js
+++ src/app/redux/features/user/userThunks.js
@@ -1,13 +1,25 @@
 "use client";
 
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { registerUser } from "@/api/user/postRequest";
+import { registerUser, loginUser } from "@/api/user/postRequest";
 
 export const registerUserThunk = createAsyncThunk(
   "user/registerUserThunk",
   async (formData, thunkAPI) => {
     try {
       const userData = await registerUser(formData);
+      return userData.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message);
+    }
+  }
+);
+
+export const loginUserThunk = createAsyncThunk(
+  "user/loginUserThunk",
+  async (formData, thunkAPI) => {
+    try {
+      const userData = await loginUser(formData);
       return userData.data;
     } catch (err) {
       return thunkAPI.rejectWithValue(err.message);
