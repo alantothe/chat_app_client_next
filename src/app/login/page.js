ffff51e05b99074cd4fe61aa445b9dff02f97e7b
alantothe <rubenamalpartida@gmail.com>
2023-09-09 22:09:53 -0400

    add login page handleSubmit logic

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,9 +2,11 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-
+import { loginUserThunk } from "../redux/features/user/userThunks";
+import { useSelector, useDispatch } from "react-redux";
 const LoginPage = () => {
   const router = useRouter();
+  const dispatch = useDispatch();
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -17,6 +19,10 @@ const LoginPage = () => {
       [name]: value,
     }));
   };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(loginUserThunk(formData));
+  };
   return (
     <div className="bg-purple-700 min-h-screen flex items-center justify-center">
       <section className="bg-gray-800 p-10 rounded-lg w-96">
@@ -24,7 +30,7 @@ const LoginPage = () => {
           <h1 className="text-4xl font-bold mb-10 text-white">Login Page</h1>
           <h3 className="text-white mb-5">Welcome Back to Alan_Chat</h3>
         </header>
-        <form className="flex flex-col item-center">
+        <form onClick={handleSubmit} className="flex flex-col item-center">
           <input
             className="text-black mb-5"
             placeholder="Enter E-Mail"
