b43499be33b2eaf9c289514bec1dc4320c454207
alantothe <rubenamalpartida@gmail.com>
2023-09-09 20:12:40 -0400

    add login extra reducers

--- src/app/redux/features/user/userExtraReducers.js
+++ src/app/redux/features/user/userExtraReducers.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { registerUserThunk } from "./userThunks";
+import { registerUserThunk, loginUserThunk } from "./userThunks";
 
 export const registerUserReducers = (builder) => {
   builder
@@ -14,6 +14,23 @@ export const registerUserReducers = (builder) => {
       state.loggedInUser = action.payload.user;
     })
     .addCase(registerUserThunk.rejected, (state, action) => {
+      state.status = "failed";
+      state.error = action.payload;
+    });
+};
+
+export const loginUserReducers = (builder) => {
+  builder
+    .addCase(loginUserThunk.pending, (state) => {
+      state.status = "loading";
+      state.error = null;
+    })
+    .addCase(loginUserThunk.fulfilled, (state, action) => {
+      state.status = "succeeded";
+      console.log("Payload:", action.payload);
+      state.loggedInUser = action.payload.user;
+    })
+    .addCase(loginUserThunk.rejected, (state, action) => {
       state.status = "failed";
       state.error = action.payload;
     });
