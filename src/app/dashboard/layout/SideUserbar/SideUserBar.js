cff3e6d3b667d18c0ba0ac6efef612d3c6cd5cca
alantothe <rubenamalpartida@gmail.com>
2023-09-10 01:09:50 -0400

    add exit icon dialog box and logic for signout on confirm

--- src/app/dashboard/layout/SideUserbar/SideUserBar.js
+++ src/app/dashboard/layout/SideUserbar/SideUserBar.js
@@ -1,12 +1,16 @@
-import { createElement } from "react";
+import { createElement, useState } from "react";
 import UserProfile from "@/app/utils/components/UserProfile";
 import {
   ArrowSmallLeftIcon,
   UserCircleIcon,
   UserPlusIcon,
 } from "@heroicons/react/24/outline";
+import { SignOutDialog } from "./components/SignOutDialog";
 
 const SideUserBar = ({ loggedInUser }) => {
+  let [dialogOpen, setDialogOpen] = useState(false);
+  const toggleDialog = () => setDialogOpen((prev) => !prev);
+
   return (
     <div
       className="h-full text-white flex flex-col items-center justify-start pt-7"
@@ -15,10 +19,19 @@ const SideUserBar = ({ loggedInUser }) => {
       {loggedInUser ? (
         <UserProfile user={{ avatar: loggedInUser.avatar }} avatarSize="50px" />
       ) : null}
-      {createElement(UserPlusIcon, { className: "h-14 w-14 pt-3" })}
+      {createElement(
+        UserPlusIcon,
+
+        { className: "h-14 w-14 pt-3" }
+      )}
 
       {createElement(UserCircleIcon, { className: "h-14 w-14 pt-3" })}
-      {createElement(ArrowSmallLeftIcon, { className: "h-14 w-14 pt-3" })}
+      {createElement(ArrowSmallLeftIcon, {
+        className: "h-14 w-14 pt-3 cursor-pointer",
+        onClick: toggleDialog,
+      })}
+
+      <SignOutDialog open={dialogOpen} toggleDialog={toggleDialog} />
     </div>
   );
 };
---
+++ src/app/dashboard/layout/SideUserbar/components/SignOutDialog.js
@@ -0,0 +1,71 @@
+import { createElement } from "react";
+
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+  Typography,
+} from "@material-tailwind/react";
+import { ArrowSmallLeftIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/navigation";
+
+export function SignOutDialog({ open, toggleDialog }) {
+  const router = useRouter();
+  const handleConfirm = () => {
+    router.push("/login");
+    toggleDialog();
+  };
+  return (
+    <>
+      <Dialog
+        open={open}
+        handler={toggleDialog}
+        className="max-w-md mx-auto mt-10 place-items-center rounded-lg shadow-lg bg-white dark:bg-zinc-900"
+      >
+        <DialogHeader className="bg-blue-gray-100 px-5 py-3 flex justify-center">
+          <Typography variant="h5" color="blue-gray">
+            Would you Like Sign Out?
+          </Typography>
+        </DialogHeader>
+        <DialogBody divider className="grid place-items-center gap-4 p-5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="h-16 w-16 text-red-500"
+          >
+            {createElement(ArrowSmallLeftIcon, {
+              className: "h-14 w-14 pt-3",
+            })}
+          </svg>
+          <Typography
+            color="red"
+            variant="h4"
+            className="font-semibold"
+          ></Typography>
+          <Typography className="text-center font-normal text-blue-gray-600">
+            Please Select Below
+          </Typography>
+        </DialogBody>
+        <DialogFooter className="border-t border-blue-gray-200 space-x-2 px-5 py-3 flex justify-center">
+          <Button
+            variant="gradient"
+            className="text-red-500"
+            onClick={handleConfirm}
+          >
+            Confirm
+          </Button>
+          <Button
+            variant="text"
+            className="text-green-500"
+            onClick={toggleDialog}
+          >
+            Cancel
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
+  );
+}
