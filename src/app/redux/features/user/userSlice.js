9c18fb62bef7069433f8c82f21269526017e0ca6
alantothe <rubenamalpartida@gmail.com>
2023-09-09 20:13:59 -0400

    update user slice with reducers and thunk

--- src/app/redux/features/user/userSlice.js
+++ src/app/redux/features/user/userSlice.js
@@ -1,18 +1,19 @@
 "use client";
 
 import { createSlice } from "@reduxjs/toolkit";
-import { registerUserReducers } from "./userExtraReducers";
+import { registerUserReducers, loginUserReducers } from "./userExtraReducers";
 
 export const userSlice = createSlice({
   name: "user",
   initialState: {
-    loggedInUser: undefined, // or loggedInUser: {}
+    loggedInUser: undefined,
     loading: false,
     error: null,
   },
   reducers: {},
   extraReducers: (builder) => {
     registerUserReducers(builder);
+    loginUserReducers(builder);
   },
 });
 
