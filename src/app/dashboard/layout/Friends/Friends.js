c9e2505f1216801d52dde2c501b917a84c2e0fa3
alantothe <rubenamalpartida@gmail.com>
2023-09-11 23:47:56 -0400

    add friend detail and css

--- src/app/dashboard/layout/Friends/Friends.js
+++ src/app/dashboard/layout/Friends/Friends.js
@@ -5,9 +5,7 @@ function Friends({ entireUser }) {
   console.log("From Friends Components");
   console.log(entireUser);
 
-  const arrayOfFriends = entireUser.detailedFriends;
-
-  console.log(arrayOfFriends);
+  const { detailedFriends = [] } = entireUser; // Default to empty array if undefined (edge case?)
 
   return (
     <div
@@ -20,10 +18,12 @@ function Friends({ entireUser }) {
       </header>
       <div className="flex-grow p-5 flex flex-col overflow-hidden">
         <div className="flex-grow overflow-auto">
-          <h2>Online - (8)</h2>
-          {arrayOfFriends.map((friend, index) => (
-            <FriendDetail friend={friend} key={index} />
-          ))}
+          <h2>Online - ({detailedFriends.length})</h2>
+          {detailedFriends &&
+            detailedFriends.length > 0 &&
+            detailedFriends.map((friend, index) => (
+              <FriendDetail friend={friend} key={index} />
+            ))}
         </div>
         <div className="flex-grow overflow-auto">
           <h2>Offline - (12)</h2>
@@ -32,5 +32,9 @@ function Friends({ entireUser }) {
     </div>
   );
 }
+
+Friends.defaultProps = {
+  entireUser: {},
+};
 
 export default Friends;
--- src/app/dashboard/layout/Friends/components/FriendDetail.js
+++ src/app/dashboard/layout/Friends/components/FriendDetail.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 function FriendDetail({ friend }) {
-  console.log("From Friend Detail");
-  console.log(friend);
-  return <div></div>;
+  return (
+    <div className="flex my-3">
+      <img
+        src={friend.avatar}
+        alt="Avatar"
+        className="w-full h-full object-cover  w-12 h-12 rounded-full overflow-hidden"
+      />
+      <h1 className="ml-3 mt-2">{friend.firstName}</h1>
+      <h1 className="ml-1 mt-2">{friend.lastName}</h1>
+    </div>
+  );
 }
 
 export default FriendDetail;
