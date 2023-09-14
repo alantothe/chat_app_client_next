430d5bd682698d8ed23db2809ff0eada7272c493
alantothe <rubenamalpartida@gmail.com>
2023-09-14 03:42:12 -0400

    created route for send message

---
+++ src/api/messages/postRequest.js
@@ -0,0 +1,10 @@
+import api from "../apiConfig";
+
+export const sendMessage = async (formData) => {
+  try {
+    const response = await api.post("messages/send/", formData);
+    return response.data;
+  } catch (error) {
+    console.log("Error: Registering user.", error);
+  }
+};
